c622f116bed6ff40f621a1bf1ba608c0887790e7
dzadzuamariami20 <dzadzua.mariami1998@gmail.com>
2025-10-12 00:27:41 +0400

    second commit

---
+++ Day 02/classwork/index.py
@@ -0,0 +1,26 @@
+print("Mariami")
+print(5)
+
+# data type - მონაცემთა ტიპები
+
+# 3 
+
+# string - არის ნებისმიერი მნიშვნელობა, რომელიც იქნება მოთავსებული "" - ბრჭყალებს შორის
+print("Mariami")
+print("group 84")
+
+
+# integer - არის ნებისმიერი მთელი რიცხვი python-ში
+print(100)
+print(84)
+
+# float - ნებისმიერი ათწილადი რიცხვი python-ში
+print(10.2)
+print(84.4)
+
+
+
+# "მარიამი"
+# "5"
+# "group 84"
+# "#@"  - string
