d0ce6065c4791e9ba497e2982038307ed25b548d
dzadzuamariami20 <dzadzua.mariami1998@gmail.com>
2025-10-12 00:42:41 +0400

    My fourth commit

---
+++ Day 03/homework/index.py
@@ -0,0 +1,45 @@
+# concatenation --> string-ის კონკატინაცია ნიშნავს დაუმატო ერთმანეთს 1-ზე მეტი string-ის ტიპის მონაცემი. გამოვიყენოთ + რათა დავუმატოთ მონაცემი მონაცემს/მნიშვნელობა მნიშვნელობას/ ცვლადი ცვლადს. 
+
+# ცვლადების შექმნა
+num1 = 1500
+num2 = -2000
+num3= 3500.60
+print(num1 + num2 + num3)
+
+# მართკუთხედის გვერდები ცვლადები/ მარკუთხედის პერიმეტრი
+length = 20
+width = 10
+print(length+length+width+width)
+print(2*length + 2*width)
+print(2*(length + width))
+
+# 3-3 მაგალითი მათემატიკურ ოპერაციებზე
+# მიმატება
+print(40+30)
+print(-12+78)
+print(-36+96.56)
+# გამოკლება
+print(-35-65)
+print(3647-257)
+print(123.47-122.47)
+# გამრავლება
+print(12*7)
+print(-15*63)
+print(-23*-2)
+# გაყოფა
+print(12/3)
+print(-156/4)
+print(-263/-14)
+
+print(89//2)
+print(-121//11)
+print(-225//-5)
+
+# ცვლადები
+level = "level 03" # STRING არის იმიტომ, რომ ამ შემთხვევაში როგორც ტექსტური ისე მსურს
+name = "Mariam"
+Surname = "Dzadzua"
+last_personalnum = "125" # STRING არის იმიტომ, რომ ამ შემთხვევაში როგორც ტექსტური ისე მსურს
+mail_domain = "@goa.edu.ge"
+
+print("Student is on"+" "+ level + "." + " " +"Student's mail is:" + " " + name+Surname+last_personalnum +mail_domain) 
