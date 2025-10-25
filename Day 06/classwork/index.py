b4634f86c4b08e2c01766831b886b2b9bee836d7
dzadzuamariami20 <dzadzua.mariami1998@gmail.com>
2025-10-25 21:15:13 +0400

    seventh commit

---
+++ Day 06/classwork/index.py
@@ -0,0 +1,35 @@
+#1 
+My_age = int(input("Text Your Age"))
+print(My_age)
+
+#2 Input --> ის, ინფორმაცია, რაც გროვდება და შედის კომპიუტერში (არა მარტო), მაგ: შეგვიძლია შევსების მიზნით მოვთხოვოთ ვინმეს რაიმე ინფოს შევსება. დაჭერითა თუ სენსორით შეგვიძლია შევიყვანოთ ინფორმაცია. მაგ:  დავალებაში vacuum cleaner რომ იყო იქ ინფორმაცია რომ შეჰქონდა სენსორის დახმარებით იყო input.
+# output --> არის ისეთი რამ, რაც გვაძლევს პასუხს, გამოაქვს ეკრანზე.  მაგ: print ფუნქციით შეგვიძლია გამოვიტანოთ ის, რაც შევიყვანეთ input-ით. მაგ: vacuum cleaner, რომ იყო იქ ხმას რომ გამოსცემდა რაღაც გაიჭედაო ეს იყო output.
+
+#3 
+a = 750
+b = 15
+
+# მიმატება
+print(a + b)
+#გამოკლება
+print(a - b)
+#გამრავლება
+print(a*b)
+#გაყოფა
+print(a/b)
+
+#4 
+favorite_color = input("Your favorite color:")
+favorite_animal = input("Your favorite animal:")
+favorite_place_country = input("Your favorite place/country:")
+print("Your favorites are: " + "color: "+ favorite_color + ", " +"animal: " + favorite_animal + ", " + "Place: " + favorite_place_country + ".")
+
+#5 
+car1 = input("your the most favorite car is: ")
+car2 = input("SECOND:")
+car3 = input("THIRD:")
+car4 = input("FOURTH:")
+
+print(car1)
+
+
---
+++ Day 06/homework/index.py
@@ -0,0 +1,20 @@
+# 2)
+maths = int(input("My Score in maths is:"))
+geography = int(input("My Score in geography is:"))
+english = int(input("My score in english is:"))
+print((maths+geography+english)/3)
+
+# 3) 
+My_age = int(input("My age is:"))
+Difference_between_mom_and_me = int(input("Difference between my mom and me is (in age):"))
+print(My_age+Difference_between_mom_and_me)
+
+# 4) Data type --> მონაცემთა ტიპი. ჩვენთვის ამ დრომდე ნასწავლი მონაცემთა ტიპებია: ტექსტური და  რიცხვითი/ციფრობრივი ტიპები. ტექსტური ტიპი: string --> შემოკლებით str; რიცხვითი/ციფრობრივი ტიპი: integer --> შემოკლებით int და float. STRING-შედგება სიტყვებისგან, წინადადებისგან ტექსტის სახით, მასზე არითმეტიკულ ფუნქციებს ვერ შეასრულებ. INTEGER -> არის მთელი რიცხვები, ხოლო FLOAT არის ათწილადი რიცხვები. 
+
+#5 
+N_MOM = input("My mom's name is:")
+N_DAD = input("My dad's name is:")
+N_BROTHER = input("My brother's name is:")
+N_HUSBAND = input("My husband's name is:")
+print("My mom"+"-"+N_MOM+","+" "+"dad"+"-"+N_DAD+","+" "+"brother"+"-"+N_BROTHER+" "+"and husband"+"-"+N_HUSBAND+" "+"organized a wonderful family celebration at our home.")
+
